13001a842ba73cbbbc56b288552dcaf295ac1ecd
Yerson Reyes <yerson.programador@gmail.com>
2020-05-26 13:57:55 -0400

    guardando tarea en firestore

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { isEmpty } from "lodash";
+import firebase from "../../utils/firebase";
+import "firebase/firestore";
 import { ReactComponent as Send } from "../../assets/original.svg";
 import "./AddTask.scss";
 
+const db = firebase.firestore(firebase);
+
 export default function AddTask() {
-  const onSubmit = () => {
-    console.log("formulario enviado.....");
+  const [task, setTask] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!isEmpty(task)) {
+      db.collection("task")
+        .add({
+          name: task,
+          completed: false,
+        })
+        .then(() => {
+          console.log("tarea creada");
+        });
+    }
   };
   return (
     <Form onSubmit={onSubmit} className="add-task">
-      <input type="text" placeholder="New Task" />
+      <input
+        type="text"
+        placeholder="New Task"
+        onChange={(e) => setTask(e.target.value)}
+      />
       <Button type="submit">
         <Send />
       </Button>
